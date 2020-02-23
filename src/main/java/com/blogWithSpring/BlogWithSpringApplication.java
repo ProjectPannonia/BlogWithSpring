f333d0acf81a060f22c8c7bd9b791735ef628217
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-02-23 17:56:26 +0100

    Initial commit

---
+++ src/main/java/com/blogWithSpring/BlogWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.blogWithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogWithSpring/BlogWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogWithSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
