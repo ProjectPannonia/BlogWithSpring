c34f4508130765c152c6f12552ca4322577be7e5
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-02-23 18:03:09 +0100

    Post class ready

--- src/main/java/com/blogWithSpring/domain/post.java
+++ src/main/java/com/blogWithSpring/domain/post.java
@@ -1,5 +1,55 @@
 package com.blogWithSpring.domain;
 
-public class post {
+import java.util.Date;
 
+public class Post {
+	
+	private String title;
+	private String content;
+	private Date posted;
+	private String author;
+	
+	public Post(String title,String content,String author) {
+		this.title = title;
+		this.content = content;
+		this.author = author;
+		this.posted = new Date();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getPosted() {
+		return posted;
+	}
+
+	public void setPosted(Date posted) {
+		this.posted = posted;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [title=" + title + "]";
+	}
 }
